d15ba0ead7aee45685544ac8e654d2d17a5d98c6
cjw <1830879724@qq.com>
2021-07-15 16:12:13 +0800

    职位管理功能实现

--- yeb-server/src/main/java/com/cjw/server/pojo/Position.java
+++ yeb-server/src/main/java/com/cjw/server/pojo/Position.java
@@ -1,10 +1,13 @@
 package com.cjw.server.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -36,6 +39,8 @@ public class Position implements Serializable {
     private String name;
 
     @ApiModelProperty(value = "创建时间")
+    @TableField("createDate")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "Asia/Shanghai")
     private LocalDateTime createDate;
 
     @ApiModelProperty(value = "是否启用")
