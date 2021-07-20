745e2887a6942a36206b2001f3699e82947cb986
cjw <1830879724@qq.com>
2021-07-20 15:24:22 +0800

    操作员更新及删除操作

--- yeb-server/src/main/java/com/cjw/server/controller/AdminController.java
+++ yeb-server/src/main/java/com/cjw/server/controller/AdminController.java
@@ -2,13 +2,11 @@ package com.cjw.server.controller;
 
 
 import com.cjw.server.pojo.Admin;
+import com.cjw.server.pojo.RespBean;
 import com.cjw.server.service.IAdminService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,26 @@ public class AdminController {
     public List<Admin> getAllAdmin(String keywords){
         return adminService.getAllAdmin(keywords);
     }
+
+    @ApiOperation(value = "更新操作员")
+    @PutMapping ("/")
+    public RespBean updateAdmin(@RequestBody Admin admin){
+        if (adminService.updateById(admin)){
+            return RespBean.success("更新成功!");
+        }
+        return  RespBean.error("更新失败!");
+    }
+
+    @ApiOperation(value = "删除操作员")
+    @DeleteMapping("/{id}")
+    public RespBean deleteAdmin(@PathVariable Integer id){
+        if (adminService.removeById(id)){
+            return RespBean.success("删除成功！");
+    }
+        return RespBean.error("删除失败!");
+
+    }
+
 
 
 }
